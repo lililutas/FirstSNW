8829e65d83499a4b0e0cda3f00740a126f13b06b
lililutas <lililutas@gmail.com>
2021-07-03 14:49:24 +0300

    first maket

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.css'
+
+
+export default function Header() {
+    return (
+        <div className='header'>
+            <div className='header__logo'>
+                <img src='https://placehold.it/50x50/333' />
+            </div>
+        </div>
+    )
+}
---
+++ src/Components/SideBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function SideBar() {
+    return (
+        <div className='sidebar'>
+            <nav className='nav'>
+                <ul className='nav__list'>
+                    <li><a href='#' className='nav__item'>Моя страница</a></li>
+                    <li><a href='#' className='nav__item'>Новости</a></li>
+                    <li><a href='#' className='nav__item'>Чот-там</a></li>
+                    <li><a href='#' className='nav__item'>Чот-там</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
