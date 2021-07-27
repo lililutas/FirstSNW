c07d1f651418657f7dc49f253f3f179ce5c3e398
lililutas <lililutas@gmail.com>
2021-07-27 12:03:22 +0300

    add dialogs

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 export default function Post() {
     return (
-        <div>
-            
+        <div className='post'>
+            <div className='post__header'>
+                <div className='post__image'>
+
+                </div>
+                <div className='post__info'>
+                    
+                </div>
+            </div>
+            <div className='post__content'>
+
+            </div>
         </div>
     )
 }
