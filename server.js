450d1e6904b2c0114ac485feb0bf183bf7aa00fd
lililutas <lililutas@gmail.com>
2021-09-30 16:17:50 +0300

    fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const path = require('path');
 
-const PORT = process.env.PORT || 3000;
+const PORT =  3000;
 
 const app = express();
 app.use(express.static(__dirname));
